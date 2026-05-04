c7b4cf315468e755f31fcb6203b8c0a0d9d32f36
Daniela Silva <168598759+danicretense@users.noreply.github.com>
2026-05-04 23:56:41 +0000

    Criada lab7

---
+++ lab7/models/Empresa.js
@@ -0,0 +1,7 @@
+class Empresa{
+    constructor(id, nome){
+        this.id=id
+        this.nome=nome
+    }
+}
+module.exports= Empresa
---
+++ lab7/models/jogo.js
@@ -0,0 +1,10 @@
+class Jogo{
+    costructor(id,  nome, categoria, ano, fkEmpresa){
+        this.id=id
+        this.nome=nome
+        this.categoria= categoria
+        this.ano=ano
+        this.fkEmpresa= fkEmpresa
+    }
+}
+module.exports=Jogo
